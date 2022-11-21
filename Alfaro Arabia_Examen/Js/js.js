c687f30aeb2a83386fc66179093827a8b53eb905
Angelus <100987588+Riley141@users.noreply.github.com>
2022-11-21 14:47:56 -0600

    Update js.js

--- Alfaro Arabia_Examen/Js/js.js
+++ Alfaro Arabia_Examen/Js/js.js
@@ -1,4 +1,4 @@
-function pl() 
+function pl((X, Y)) 
 {
 
 var X=document.getElementById('corx').value;
@@ -7,7 +7,10 @@ var X=document.getElementById('corx').value;
 var Y=document.getElementById('cory').value;
 
 
-var r;
+   var r;
+   var d;
+   var resul;
+     
 
 r= Math.pow(2)
 
@@ -15,45 +18,15 @@ r= Math.pow(2)
 X= Math.pow(X, 2)
 Y= Math.pow(X, 2)
 
-r=(X+Y)
 
-
-for(let t=0;t<X; t++){
-    let cosT = Math.cos((t/360) * (Math.PI*2))
-    let sinT = Math.tan((t/360) * (Math.PI*2))
-    let X = cosT * 40;
-    let Y = sinT * 40;
-    X = cosT * 60;
-    Y = sinT * 60;
-    
-};
-
-
-let resultado=1;
-
-for (let i=0; i<e; i++) 
-{
-   
-}
-
-return resultado;
-}
+    d = Math.sqrt(x*x + y*y)
+    r = Math.atan2(y,x) 
+    resul = { distance:distance, radians:radians }
+    return resul;
 
 document.getElementById('resul').resultado;
 
 
-const caja = document.querySelector("#caja");
-const texto = document.querySelector("#texto");
 
-
-//evento
-caja.addEventListener("click", cambiarColor);
-
-
-//funcion cambiar color
-function cambiarColor(){
-  caja.classList.toggle("bg-primary");
-  caja.classList.toggle('bg-success');
-  texto.classList.toggle('text-warning');
-  texto.classList.toggle('text-white');
+  
 }
